6bc0dff94e46d15fd143351c913179dd9a60a6ac
POINSOT ANTOINE p1607390 <p1607390@b710l0412.univ-lyon1.fr>
2018-09-17 11:16:14 +0200

    Beginning of login form for school

--- Certification/application.py
+++ Certification/application.py
@@ -1,18 +1,23 @@
 #coding: utf-8
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 webapp = Flask(__name__)
 
 class App:
     def __init__(self):
         pass
 
-    @webapp.route('/index.html')
-    @webapp.route('/index')
-    @webapp.route('/')
+    @webapp.route('/index.html', methods=['GET'])
+    @webapp.route('/index', methods=['GET'])
+    @webapp.route('/', methods=['GET'])
     def index():
         return render_template('index.html', js_folder=url_for('static', filename='js'), img_folder=url_for('static', filename='img'), css_folder=url_for('static', filename='css'))
 
+    @webapp.route('/school_login', methods=['POST'])
+    def school_login():
+        school_seed = request.form['secret']
+        return render_template('index.html', js_folder=url_for('static', filename='js'), img_folder=url_for('static', filename='img'), css_folder=url_for('static', filename='css'), school=1)
+    
     def run(self, host):
         webapp.run(host)
 
